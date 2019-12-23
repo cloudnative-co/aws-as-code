dc305a8e17f0abcc683b0b23520aa892477cb4cc
ISOBE Kazuhiko <muramasa64@gmail.com>
2019-12-23 17:17:23 +0900

    add: VPC Stack

--- pingfederate/lib/pingfederate-stack.ts
+++ pingfederate/lib/pingfederate-stack.ts
@@ -1,9 +1,19 @@
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2')
+import { SubnetType, Subnet } from '@aws-cdk/aws-ec2';
 
 export class PingfederateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      cidr: '10.100.0.0/16',
+      subnetConfiguration: [
+        {
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC
+        }
+      ]
+    });
   }
 }
