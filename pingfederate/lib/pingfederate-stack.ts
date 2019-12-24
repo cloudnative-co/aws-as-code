b126634a10fef3503430d76533b2b6c398313e07
ISOBE Kazuhiko <muramasa64@gmail.com>
2019-12-24 15:36:15 +0900

    add: EC2 Instances

--- pingfederate/lib/pingfederate-stack.ts
+++ pingfederate/lib/pingfederate-stack.ts
@@ -1,12 +1,12 @@
 import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2')
-import { SubnetType, Subnet } from '@aws-cdk/aws-ec2';
+import { SubnetType, Subnet, CfnEC2Fleet, InstanceSize } from '@aws-cdk/aws-ec2';
 
 export class PingfederateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'VPC', {
+    const my_vpc = new ec2.Vpc(this, 'VPC', {
       cidr: '10.100.0.0/16',
       subnetConfiguration: [
         {
@@ -14,6 +14,18 @@ export class PingfederateStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PUBLIC
         }
       ]
+    });
+
+    const adds = new ec2.Instance(this, 'adds', {
+      vpc: my_vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3A, ec2.InstanceSize.LARGE),
+      machineImage: new ec2.WindowsImage(ec2.WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_CORE_BASE)
+    });
+
+    const pingfederate = new ec2.Instance(this, 'pingfederate', {
+      vpc: my_vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3A, ec2.InstanceSize.MEDIUM),
+      machineImage: new ec2.WindowsImage(ec2.WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_CORE_BASE)
     });
   }
 }
