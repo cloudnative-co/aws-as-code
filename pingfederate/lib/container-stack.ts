c21c840ef36efddb1fc467cc18cadb47b1683310
ISOBE Kazuhiko <muramasa64@gmail.com>
2020-02-02 00:42:00 +0900

    [WIP] update: ContaineStack

--- pingfederate/lib/container-stack.ts
+++ pingfederate/lib/container-stack.ts
@@ -1,6 +1,7 @@
 import cdk = require('@aws-cdk/core')
 import ecs = require('@aws-cdk/aws-ecs')
 import ec2 = require('@aws-cdk/aws-ec2')
+import autoscaling = require('@aws-cdk/aws-autoscaling')
 import { IVpc } from '@aws-cdk/aws-ec2'
 
 interface ContainerStackProps extends cdk.StackProps {
@@ -15,17 +16,17 @@ export class ContainerStack extends cdk.Stack {
             clusterName: "PingFederate",
             vpc: props.vpc
         })
-        cluster.addCapacity('spot' {
+
+        cluster.addCapacity('PingFederate', {
             maxCapacity: 1,
             minCapacity: 1,
-            desiredCapacity: 1,
-            instanceType: new ec2.InstanceType('')
+            instanceType: new ec2.InstanceType('t2.medium')
         })
 
         const taskDefinition = new ecs.Ec2TaskDefinition(this, 'PingFederateTask')
         taskDefinition.addContainer('PFContainer', {
             image: ecs.ContainerImage.fromRegistry("pingidentity/pingfederate:9.3.3"),
-            memoryLimitMiB: 512
+            memoryLimitMiB: 2048
         })
 
         const ecsService = new ecs.Ec2Service(this, 'Service', {
