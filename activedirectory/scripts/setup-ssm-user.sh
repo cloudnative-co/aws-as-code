1e76af4cf53d5840c95e509a3379b57dc4db1529
ISOBE Kazuhiko <muramasa64@gmail.com>
2020-03-04 22:33:10 +0900

    fix: reset admin password

--- activedirectory/scripts/setup-ssm-user.sh
+++ activedirectory/scripts/setup-ssm-user.sh
@@ -6,7 +6,7 @@ echo "Setup SSM User for ${ADDS_INSTANCE_ID}"
 SETUP_SSM_USER_DOCUMENT_NAME=`aws ssm list-documents --query 'DocumentIdentifiers[?Tags[?Key==\`Name\`].Value|[0]==\`setup-ssm-user\`][]|[0].Name'|tr -d '"'`
 echo $SETUP_SSM_USER_DOCUMENT_NAME
 
-RESULT=`aws ssm send-command --instance-ids "${ADDS_INSTANCE_ID}" --document-name "${SETUP_SSM_USER_DOCUMENT_NAME}" --cloud-watch-output-config '{"CloudWatchOutputEnabled":true}'`
+RESULT=`aws ssm send-command --instance-ids "${ADDS_INSTANCE_ID}" --document-name "${SETUP_SSM_USER_DOCUMENT_NAME}" --parameters "domainNetBiosName=${CDK_MY_DOMAIN_NETBIOS_NAME}" --cloud-watch-output-config '{"CloudWatchOutputEnabled":true}'`
 
 if [ -x `which jq` ]; then
   echo $RESULT | jq ". | { CommandId: .Command.CommandId, InstanceIds: .Command.InstanceIds }"
