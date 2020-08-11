2801727ab97e5d933eb28b0eaac60f2d26248565
Isobe Kazuhiko <muramasa64@gmail.com>
2020-08-11 16:36:49 +0900

    add environment parameter for AWS Account and region

--- activedirectory/bin/activedirectory.ts
+++ activedirectory/bin/activedirectory.ts
@@ -8,17 +8,22 @@ import { ManagementStack } from '../lib/management-stack';
 import { SecretStack } from '../lib/secrets-stack';
 import { DhcpStack } from '../lib/dhcpoptionset-stack';
 
+const myenv = {
+    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
+}
 const app = new cdk.App();
-new SecretStack(app, 'AddsSecretStack');
-new ManagementStack(app, 'AddsManagementStack');
-const identityStack = new IdentityStack(app, 'AddsIdentityStack');
-const networkStack = new NetworkStack(app, 'AddsNetworkStack');
+new SecretStack(app, 'AddsSecretStack', { env: myenv });
+new ManagementStack(app, 'AddsManagementStack', { env: myenv });
+const identityStack = new IdentityStack(app, 'AddsIdentityStack', { env: myenv });
+const networkStack = new NetworkStack(app, 'AddsNetworkStack', { env: myenv });
 const computerStack = new ComputerStack(app, 'AddsComputerStack', {
     vpc: networkStack.vpc,
     addsRole: identityStack.addsRole,
+    env: myenv
 });
 new DhcpStack(app, 'AddsDhcpStack', {
     vpc: networkStack.vpc,
-    addsPrivateIpAddress: computerStack.addsPrivateIpAddress
+    addsPrivateIpAddress: computerStack.addsPrivateIpAddress,
+    env: myenv
 });
-
